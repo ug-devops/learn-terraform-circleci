c25453b6db2c41e6c0e7787e36970fde44428197
Usman <ughani98@gmail.com>
2023-08-28 18:23:34 +0500

    Update variable definitions

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+terraform {
+  cloud {
+    organization = "ug-hcp"
+
+    workspaces {
+      name = "learn-terraform-circleci"
+    }
+  }
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.4.0"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
 provider "aws" {
   region = var.region
 
