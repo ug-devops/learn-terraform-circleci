4088af5eca323238d86454fa9c7de536e6532ae6
Usman <ughani98@gmail.com>
2023-08-28 14:53:27 +0500

    Update variable definitions

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,16 @@
 terraform {
-  cloud {}
+  cloud {
+    organization = "ug-hcp"
 
+    workspaces {
+      name = "learn-terraform-circleci"
+    }
+  }
+}
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.4.0"
     }
   }
-
   required_version = ">= 1.2.0"
-}
